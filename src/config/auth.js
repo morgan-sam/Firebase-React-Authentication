13291dd50ee49ee59ed5b57783282f050cba1d02
Sam <samuel_morgan_@outlook.com>
2020-06-26 15:19:11 +0100

    Loading state + display

--- src/config/auth.js
+++ src/config/auth.js
@@ -5,6 +5,22 @@ export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
 	const [ currentUser, setCurrentUser ] = useState(null);
-	useEffect(() => app.auth().onAuthStateChanged(setCurrentUser), []);
-	return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>;
+	const [ loading, setLoading ] = useState(true);
+	useEffect(
+		() =>
+			app.auth().onAuthStateChanged((user) => {
+				setCurrentUser(user);
+				setLoading(false);
+			}),
+		[]
+	);
+	if (loading)
+		return (
+			<div className="App">
+				<div className="accountInput">
+					<h2 className="title">Loading...</h2>
+				</div>
+			</div>
+		);
+	else return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>;
 };

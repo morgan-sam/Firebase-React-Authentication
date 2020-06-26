3d323a611685392ed8ae83607f70d5bc013041b3
Sam <samuel_morgan_@outlook.com>
2020-06-26 18:58:57 +0100

    Home shows image, click for new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import app from 'config/firebase';
 
 const Home = () => {
+	const newImage = async () => {
+		try {
+			const raw = await fetch(`https://picsum.photos/200/300`);
+			setImg(raw.url);
+		} catch (error) {
+			alert(error);
+		}
+	};
+	const [ img, setImg ] = useState();
+	useEffect(() => newImage(), []);
 	return (
 		<div className="home">
 			<h1 className="title">Home</h1>
+			<img style={{ height: '300px', width: '200px', margin: '1rem' }} src={img} />
+			<button onClick={() => newImage()}>New Image</button>
 			<button onClick={() => app.auth().signOut()}>Sign Out</button>
 		</div>
 	);
--- src/js/utlity.js
+++ src/js/utlity.js
@@ -2,3 +2,5 @@ export const capitalizeFirstLetter = (string) => {
 	if (typeof string !== 'string') return '';
 	else return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+export const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;

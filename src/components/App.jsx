d9d11b10a6028892d4e2b4aacfcf1448b215012c
Sam <samuel_morgan_@outlook.com>
2020-06-26 13:45:45 +0100

    Created PrivateRoute comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from 'components/Home';
+import { AuthProvider } from 'config/auth';
+import PrivateRoute from 'components/PrivateRoute';
 
 const App = () => {
 	return (
-		<Router>
-			<div className="App">
-				<Route exact path="/" component={Home} />
-				<Route exact path="/" component={null} />
-				<Route exact path="/" component={null} />
-			</div>
-		</Router>
+		<AuthProvider>
+			<Router>
+				<div className="App">
+					<PrivateRoute exact path="/" component={Home} />
+					<Route exact path="/" component={null} />
+					<Route exact path="/" component={null} />
+				</div>
+			</Router>
+		</AuthProvider>
 	);
 };
 
